e20efcfc33274e1719cf2876fa7e0a4a0c790082
saladin76 <salahelnabtity@gmail.com>
2026-07-23 21:58:10 +0300

    Support legacy brand message templates redirect

---
+++ app/(dashboard)/dashboard/brand/message-templates/page.tsx
@@ -0,0 +1,7 @@
+import { redirect } from "next/navigation";
+
+export const dynamic = "force-dynamic";
+
+export default function BrandMessageTemplatesPage() {
+  redirect("/dashboard/operations/communication/templates?tab=legacy-frameworks");
+}
